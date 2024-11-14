db43ce7d522781b82cbcae448c29f0ea11c88171
Valentin Kim <valentink@yandex.ru>
2024-11-14 17:15:49 +0500

    добавлено логирование при авторизации для выясления багов в части авторизации

--- server.py
+++ server.py
@@ -26,12 +26,16 @@ async def verify_token_with_auth_server(token):
     try:
         url = "https://backoffice.daribar.com/api/v1/users"
         headers = {"Authorization": f"Bearer {token}"}
+        logger.info(f"Token: {token}")
         async with httpx.AsyncClient() as client:
             response = await client.get(url, headers=headers)
             if response.status_code == 200:
                 logger.info(f"responseJWT: {response.json()}")
                 return response.json()  # Возвращаем данные пользователя
             else:
+                logger.error(
+                    f"Error: {response.status_code} - {response.text}"
+                )
                 return None
     except Exception as e:
         logger.error(f"Error verifying token: {e}")
